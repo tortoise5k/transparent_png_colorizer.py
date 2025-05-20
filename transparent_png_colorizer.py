2e57b97ffa1cbfa482fdf1dccfda4f49705e169e
tortoise5k <57031455+tortoise5k@users.noreply.github.com>
2025-05-20 20:49:23 +0900

    Add: 背景色付け画像とペアになる画像/キャプションデータをコピー

--- transparent_png_colorizer.py
+++ transparent_png_colorizer.py
@@ -1,4 +1,5 @@
 import argparse
+import shutil
 from pathlib import Path
 
 import numpy as np
@@ -83,6 +84,67 @@ def apply_color_to_transparent_png(png_path, color, output_path):
     result_img.save(output_path)
 
 
+def check_dataset(image_files: list[Path], image_captions: list[Path]) -> None:
+    len_img = len(image_files)
+    len_txt = len(image_captions)
+    if len_img == 0 or len_txt == 0:
+        raise ValueError("画像ファイルとキャプションファイルのペアが見つかりません。")
+    if len_img != len_txt:
+        raise ValueError("画像ファイルとキャプションファイルの数が一致しません。")
+
+    img_txt_pairs = [
+        (img, txt)
+        for img, txt in zip(image_files, image_captions)
+        if img.stem == txt.stem
+    ]
+    if len_img != len(img_txt_pairs):
+        raise ValueError("画像ファイルとキャプションファイルの名前が一致しません。")
+
+    return None
+
+
+def copy_input_files(
+    copy_input_dir: Path, copy_output_dir: Path, num_colors: int, digits: int
+) -> None:
+    """
+    入力ディレクトリの画像ファイル、キャプションファイルを出力ディレクトリにコピーする
+    """
+    # 出力ディレクトリを作成
+    copy_output_dir.mkdir(parents=True, exist_ok=True)
+
+    # 画像ファイルの拡張子リスト
+    image_exts = [".png", ".jpg", ".jpeg", ".bmp", ".gif", ".tiff", ".webp"]
+
+    # input dirのすべてのファイルを画像ファイルか判定してリスト化、ソート (0001..000N)
+    image_files = sorted(
+        [f for f in copy_input_dir.glob("*") if f.suffix.lower() in image_exts]
+    )
+    # キャプションファイルの拡張子リスト
+    image_captions = sorted(copy_input_dir.glob("*.txt"))
+
+    check_dataset(image_files, image_captions)
+
+    img_txt_pairs = [
+        (img, txt)
+        for img, txt in zip(image_files, image_captions)
+        if img.stem == txt.stem
+    ]
+
+    # 画像ファイルを出力ディレクトリにコピー(num_colors分循環してコピー)
+    for i in range(num_colors):
+        # 画像ファイルのインデックス（循環させる）
+        pair_index = i % len(img_txt_pairs)
+        img_path, txt_path = img_txt_pairs[pair_index]
+
+        # 出力ファイル名の形式を設定
+        img_output_path = copy_output_dir / f"{str(i + 1).zfill(digits)}.png"
+        txt_output_path = copy_output_dir / f"{str(i + 1).zfill(digits)}.txt"
+
+        # ファイルをコピー
+        shutil.copy(img_path, img_output_path)
+        shutil.copy(txt_path, txt_output_path)
+
+
 def main():
     parser = argparse.ArgumentParser(description="透過PNGに背景色を適用するプログラム")
     parser.add_argument("input_dir", help="透過PNGファイルのディレクトリ")
@@ -94,6 +156,14 @@ def main():
     parser.add_argument(
         "--add_disturbance", action="store_true", help="揺らぎを加える場合に指定"
     )
+    parser.add_argument(
+        "--copy_input_dir",
+        help="入力ファイルを出力ディレクトリにコピーする場合に指定",
+    )
+    parser.add_argument(
+        "--copy_output_dir",
+        help="入力ファイルを出力ディレクトリにコピーする場合に指定",
+    )
     args = parser.parse_args()
     input_dir = Path(args.input_dir)
     output_dir = Path(args.output_dir)
@@ -102,7 +172,7 @@ def main():
         output_dir.mkdir(parents=True, exist_ok=True)
 
     # 透過PNGファイルのリストを取得
-    png_files = [f for f in input_dir.iterdir() if f.lower().endswith(".png")]
+    png_files = [f.name for f in input_dir.iterdir() if f.suffix.lower() == ".png"]
 
     # 透過PNGの数
     num_pngs = len(png_files)
@@ -139,6 +209,15 @@ def main():
 
         print(
             f"処理完了: {output_filename} - PNG: {png_files[png_index]}, 色: RGB{color}"
+        )
+
+    # 入力ディレクトリの画像ファイル、キャプションファイルを出力ディレクトリにコピー、足りない場合はindex循環してコピー
+    if args.copy_input_dir:
+        copy_input_files(
+            Path(args.copy_input_dir),
+            Path(args.copy_output_dir),
+            args.num_colors,
+            args.digits,
         )
 
 
